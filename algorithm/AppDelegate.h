05496750074b884494ce367626c5bf2e8e52fac0
TruongDoanh <nguyentruongdoanh95@gmail.com>
2017-06-18 13:01:51 +0700

    Initial Commit

---
+++ algorithm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  algorithm
+//
+//  Created by Godfather on 6/18/17.
+//  Copyright © 2017 Godfather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
